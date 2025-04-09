e688dfeed6e9848c7638d245669dec1002147b58
Mariusz Woźnica <mariusz.adam.woznica@gmail.com>
2025-04-09 18:57:08 +0200

    UI project set-up

---
+++ src/FractalImagesGenerator.UI/ViewModels/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace FractalImagesGenerator.UI.ViewModels;
+
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
