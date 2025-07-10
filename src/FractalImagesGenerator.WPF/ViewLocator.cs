bace4e8262c7da20ed82a8deae75c5a7006484dd
mariusz <mariusz.adam.woznica@gmail.com>
2025-07-10 14:32:58 +0100

    modify the navigation by incorporating a viewLocator converter

---
+++ src/FractalImagesGenerator.WPF/ViewLocator.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace FractalImagesGenerator.WPF;
+
+internal class ViewLocator : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return null;
+
+        var viewName = value.GetType().FullName!.Replace(
+            "ViewModel", "View", StringComparison.InvariantCulture);
+        var type = Type.GetType(viewName);
+
+        if (type == null)
+            return null;
+
+        var control = (Control)Activator.CreateInstance(type)!;
+        control.DataContext = value;
+        return control;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
